b6cae36edde5950e71f45a28fcc9c0379b840554
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-03-29 13:38:37 -0300

    Create Media.vue

---
+++ src/views/cloud/Media.vue
@@ -0,0 +1,16 @@
+<template>
+  <template-sub-page>
+    <template slot="title">
+      Medios
+    </template>
+    <template slot="actions">
+      <el-button-group>
+        <el-button>
+          <img style="width: 24px;" src="https://firebasestorage.googleapis.com/v0/b/one-sig-uy.appspot.com/o/console%2Fassets%2Ficons%2Fcolor%2Fsvg%2Fplus.svg?alt=media&token=4a9d0286-fe5e-4d35-8faf-bb9afd0e0f2e" alt="">
+        </el-button>
+      </el-button-group>
+    </template>
+    <template slot="content">
+    </template>
+  </template-sub-page>
+</template>
