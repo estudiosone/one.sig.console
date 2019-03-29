86e196cef02ac2e381e218c133202c308ffbaef0
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-03-29 12:21:53 -0300

    Se crearon páginas para el módulo Configuración

---
+++ src/views/Settings.vue
@@ -0,0 +1,43 @@
+<template>
+  <template-page class="page-settings">
+    <template slot="breadcrumb">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>SIG</el-breadcrumb-item>
+        <el-breadcrumb-item>Configuración</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="route.startsWith('/settings/basic-data')">Datos básicos</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="route.startsWith('/settings/basic-data/people')">Personas</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="route.startsWith('/settings/basic-data/people/people')">Personas</el-breadcrumb-item>
+      </el-breadcrumb>
+    </template>
+    <template slot="title">Configuración</template>
+    <template slot="menu">
+      <el-menu
+        class="el-menu-demo"
+        mode="horizontal"
+        :default-active="route"
+        :router="true">
+        <el-menu-item index="/settings">Dashboard</el-menu-item>
+        <el-submenu index="/settings/basic-data">
+          <template slot="title">Datos básicos</template>
+          <el-submenu index="/settings/basic-data/people">
+            <template slot="title">Personas</template>
+            <el-menu-item index="/settings/basic-data/people/people">Personas</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+      </el-menu>
+    </template>
+  </template-page>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log(this.$route.path);
+  },
+  computed: {
+    route() {
+      return this.$route.path;
+    },
+  },
+};
+</script>
---
+++ src/views/settings/basic-data/people/People.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="sub-page page-settings-basicData-people-people">
+    <span class="sub-title">Personas</span>
+    <el-table
+      style="width: 100%;"
+      empty-text="Upss! aca no hay nadie...!!!"
+      :data="tableData">
+      <el-table-column
+        type="selection"/>
+      <el-table-column
+        label="Nombre"
+        property="name"/>
+      <el-table-column
+        label="Apellido"
+        property="lastName"/>
+      <el-table-column
+        label="Nombre"
+        property="name"/>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+
+      ],
+      multipleSelection: [
+
+      ],
+    };
+  },
+};
+</script>
