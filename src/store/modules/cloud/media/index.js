c4eb89522c156c2a6854a7e5d745e1585c1d84b7
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-03-30 09:16:54 -0300

    Primera etapa de creacion Vuex

---
+++ src/store/modules/cloud/media/index.js
@@ -0,0 +1,13 @@
+import state from './state';
+import mutations from './mutations';
+import actions from './actions';
+import getters from './getters';
+import modules from './modules';
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+  modules,
+};
